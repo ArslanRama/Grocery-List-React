f2164ed23a0ba0e40f485f5bd3d325e3634e1997
A.Ramazan Arslan <arslanrama84@gmail.com>
2023-05-31 11:08:34 +0200

    adjust item

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import "./App.css";
 import Item from "./components/Item";
 import { v4 as uuidv4 } from "uuid";
 
+// This arr function retrieves data from the localStorage using the key "data". 
+// If there is data stored, it parses the data from a JSON string format to an array format. 
+// Otherwise, it returns an empty array.
 const arr = () => {
   const data = localStorage.getItem("data");
   return data ? JSON.parse(data) : [];
 };
 
+// The App function is defined, which represents the main component of the application. 
+// It uses the useState hook to define two state variables: item and list. 
 function App() {
+  // The item state variable holds the value of the input field where users can enter new grocery items. 
   const [item, setItem] = useState("");
+  // The list state variable holds an array of grocery items.
   const [list, setList] = useState(arr);
-
+// The handleSubmit is called when the user submits the form, which occurs when they click the "Add Items" button or press the Enter key. 
+// It takes an event (e) as an argument and prevents the default form submission behavior. 
+  
   const handleSubmit = (e) => {
     e.preventDefault();
+    // It then checks if the item state variable is not empty and its length is less than or equal to 25 characters.
+    // If both conditions are true, a new item object is created with a unique id generated using the uuidv4 function, the item value from the state, and a complete property set to false.
     if (item && item.length <= 25) {
+      // The new item is added to the list state using the setList function and the item state is reset to an empty string using the setItem function. 
       const newItem = {
         id: uuidv4(),
         item: item,
@@ -24,11 +36,12 @@ function App() {
       setItem("");
     }
   };
-
+// It uses the localStorage.setItem method to store the list array as a JSON string using the key "data".
   useEffect(() => {
     localStorage.setItem("data", JSON.stringify(list));
   }, [list]);
 
+// The handleChange function takes an event (e) as an argument and updates the item state variable with the value from the input field using the setItem function.
   const handleChange = (e) => {
     setItem(e.target.value);
   };
@@ -50,6 +63,10 @@ function App() {
         <br />
         <br />
       </form>
+
+      {/* Inside the div, the list state array is mapped over to render individual Item components. 
+      Each Item component receives props such as key, id, item, list, setList, complete, and setItem. 
+      These props are used to display and manage each grocery item in the list. */}
       <div>
         {list.map((c) => (
           <Item
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
+/* It allows the user to update the item's text, mark it as complete, and delete it from the list. 
+The component receives and modifies the list state through the list and setList props, 
+and it manages the complete state of the item through the complete prop. */
+
 import React from "react";
 import "./Item.css";
+import { FaTrash } from "react-icons/fa";
 
+// These props are used to display and manage a single item in a list.
 const Item = ({ id, item, list, setList, complete }) => {
+  // The remove function takes an id as an argument and is called when the user clicks on the delete button. It uses the setList function, which is passed as a prop, to update the list state by filtering out the item with the specified id.
   const remove = (id) => {
     setList(list.filter((el) => el.id !== id));
   };
 
+  // The handleComplete function takes an id as an argument and is called when the user clicks on the complete button. It uses the setList function to update the list state by mapping over the existing list and toggling the complete property of the item with the specified id.
   const handleComplete = (id) => {
     setList(
       list.map((item) => {
@@ -20,9 +28,11 @@ const Item = ({ id, item, list, setList, complete }) => {
     );
   };
 
+  // The handleItem function is called when the user types in the input field. It takes an event (e) as an argument and extracts the updated item value from the event target's value.
   const handleItem = (e) => {
     const updatedItem = e.target.value;
 
+    // If the length of the updated item is less than or equal to 25 characters, it uses the setList function to update the list state by mapping over the existing list and updating the item property of the item with the specified id.
     if (updatedItem.length <= 25) {
       setList(
         list.map((el) => {
@@ -39,26 +49,30 @@ const Item = ({ id, item, list, setList, complete }) => {
   };
 
   return (
+    // If complete is true, the resulting element will have both the "item" and "complete" classes.
+    // If complete is false or falsy, the element will only have the "item" class
     <div className={`item ${complete ? "complete" : ""}`}>
+      <input type="text" value={item} onChange={handleItem} />
       <input
-        type="text"
-        value={item}
-        onChange={handleItem}
-      />
-      <img
-        onClick={() => handleComplete(id)}
-        src="https://img.icons8.com/offices/40/000000/checked-2--v2.png"
-        alt="Complete task"
+        type="checkbox"
+        checked={complete}
+        onChange={() => handleComplete(id)}
         className="item-icon"
-        style={{ width: "24px", height: "24px", marginRight: "5px" }} 
+        style={{ marginRight: "5px" }}
       />
-      <img
+      <button
         onClick={() => remove(id)}
-        src="https://img.icons8.com/color/48/000000/trash.png"
-        alt="Delete"
         className="item-icon"
-        style={{ width: "24px", height: "24px" }} 
-      />
+        style={{
+          width: "24px",
+          height: "24px",
+          padding: 0,
+          border: "none",
+          background: "none",
+        }}
+      >
+        <FaTrash style={{ width: "100%", height: "100%" }} />
+      </button>
     </div>
   );
 };
